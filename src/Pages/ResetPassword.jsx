5f00aaa045c1573c30fd0e43bc4a254db40d11e1
premasanub <premakumaran15@gmail.com>
2026-01-13 01:19:51 +0530

    error cleared

--- src/Pages/ResetPassword.jsx
+++ src/Pages/ResetPassword.jsx
@@ -2,7 +2,7 @@ import React, { useState } from "react";
 import { useNavigate, useParams } from "react-router-dom";
 import { toast } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
-import api from "../Services/api";
+import api from "../Services/Api";
 const ResetPassword = () => {
   const navigate = useNavigate();
   const [password, setPassword] = useState("");
