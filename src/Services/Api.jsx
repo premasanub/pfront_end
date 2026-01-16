eedf6675ef8820a88d0badc490c3687744b6bf76
premasanub <premakumaran15@gmail.com>
2026-01-16 13:09:06 +0530

    completed

--- src/Services/Api.jsx
+++ src/Services/Api.jsx
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-    baseURL: "https://reset-password-a9lq.onrender.com"
+    baseURL: "https://reset-password-a9lq.onrender.com/api"
 });
 
 api.interceptors.request.use(
